d9ed6c01e591c3c24e7e7609f2f2a34b462db4ad
Semih Özhan <semihozhan@yandex.com>
2022-01-18 16:33:33 +0300

    CommentMap Added

---
+++ NetBlog.Data/Concreate/EntityFramework/Mappings/CommentMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NetBlog.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Data.Concreate.EntityFramework.Mappings
+{
+    public class CommentMap : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(c=>c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Text).IsRequired();
+            builder.Property(c => c.Text).HasMaxLength(1000);
+            builder.HasOne<Article>(c => c.Article).WithMany(a => a.Comments).HasForeignKey(c => c.ArticleID);
+            builder.Property(c => c.CreatedByName).IsRequired();
+            builder.Property(c => c.CreatedByName).HasMaxLength(50);
+            builder.Property(c => c.ModifiedByName).IsRequired();
+            builder.Property(c => c.ModifiedByName).HasMaxLength(50);
+            builder.Property(c => c.CreatedOn).IsRequired();
+            builder.Property(c => c.ModifiedOn).IsRequired();
+            builder.Property(c => c.IsActive).IsRequired();
+            builder.Property(c => c.IsDeleted).IsRequired();
+            builder.ToTable("Comments");
+        }
+    }
+}
--- NetBlog.Entities/Concreate/Comment.cs
+++ NetBlog.Entities/Concreate/Comment.cs
@@ -10,7 +10,7 @@ namespace NetBlog.Entities.Concreate
     public class Comment: EntityBase,IEntitiy
     {
         public string Text { get; set; }
-        public int AticleID { get; set; }
+        public int ArticleID { get; set; }
         public Article Article { get; set; }
 
     }
