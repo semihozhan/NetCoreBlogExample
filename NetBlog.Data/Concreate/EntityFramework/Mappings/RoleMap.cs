34f040018a6643af378bb11066f5d23442ba7a6e
Semih Özhan <semihozhan@yandex.com>
2022-01-18 17:35:09 +0300

    RoleMap added

---
+++ NetBlog.Data/Concreate/EntityFramework/Mappings/RoleMap.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NetBlog.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Data.Concreate.EntityFramework.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Id).ValueGeneratedOnAdd();
+            builder.Property(r => r.Name).IsRequired();
+            builder.Property(r => r.Name).HasMaxLength(30);
+            builder.Property(r => r.Description).IsRequired();
+            builder.Property(r => r.Description).HasMaxLength(500);
+
+            builder.Property(r => r.CreatedByName).IsRequired();
+            builder.Property(r => r.CreatedByName).HasMaxLength(50);
+            builder.Property(r => r.ModifiedByName).IsRequired();
+            builder.Property(r => r.ModifiedByName).HasMaxLength(50);
+            builder.Property(r => r.CreatedOn).IsRequired();
+            builder.Property(r => r.ModifiedOn).IsRequired();
+            builder.Property(r => r.IsActive).IsRequired();
+            builder.Property(r => r.IsDeleted).IsRequired();
+
+            builder.ToTable("Roles");
+        }
+    }
+}
