083bbf63341243b939606745e3894ce0d0f2e336
Semih Özhan <semihozhan@yandex.com>
2022-01-18 14:43:51 +0300

    CategoryMap added.

---
+++ NetBlog.Data/Concreate/EntityFramework/Mappings/CategoryMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NetBlog.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Data.Concreate.EntityFramework.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c=>c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Name).IsRequired();
+            builder.Property(c => c.Name).HasMaxLength(70);
+            builder.Property(c => c.Description).HasMaxLength(255);
+            builder.Property(c => c.CreatedByName).IsRequired();
+            builder.Property(c => c.CreatedByName).HasMaxLength(50);
+            builder.Property(c => c.ModifiedByName).IsRequired();
+            builder.Property(c => c.ModifiedByName).HasMaxLength(50);
+            builder.Property(c => c.CreatedOn).IsRequired();
+            builder.Property(c => c.ModifiedOn).IsRequired();
+            builder.Property(c => c.IsActive).IsRequired();
+            builder.Property(c => c.IsDeleted).IsRequired();
+            builder.ToTable("Categories");
+
+        }
+    }
+}
