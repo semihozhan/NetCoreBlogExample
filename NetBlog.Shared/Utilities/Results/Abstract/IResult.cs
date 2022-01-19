197c39a5d78c30059e57c5b5e774abcef3ad5ca8
Semih Özhan <semihozhan@yandex.com>
2022-01-19 21:42:37 +0300

    IResult

---
+++ NetBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using NetBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;  }
+        public string Message { get;  }
+        public Exception Exception { get; }
+    }
+}
