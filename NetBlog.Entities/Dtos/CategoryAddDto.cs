a55dfe1b2418d2babb26cafc1a2e70d1a012cb1b
Semih Özhan <semihozhan@yandex.com>
2022-01-20 14:35:26 +0300

    CategoryAddDto

---
+++ NetBlog.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage ="{0} Boş Olamaz")]
+        [MaxLength(70,ErrorMessage ="{0} Max {1} Karakter Olmalı")]
+        [MinLength(2,ErrorMessage ="{0} Min {0} Karakter Olmalı")]
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} Max {1} Karakter Olmalı")]
+        [MinLength(2, ErrorMessage = "{0} Min {0} Karakter Olmalı")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori Notu")]
+        [MaxLength(100, ErrorMessage = "{0} Max {1} Karakter Olmalı")]
+        [MinLength(2, ErrorMessage = "{0} Min {0} Karakter Olmalı")]
+        public string Note { get; set; }
+
+        [DisplayName("Kategori Aktif mi ?")]
+        [Required(ErrorMessage = "{0} Boş Olamaz")]
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ NetBlog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,26 @@
+﻿using NetBlog.Entities.Concreate;
+using NetBlog.Entities.Dtos;
+using NetBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryID);
+
+        Task<IDataResult<IList<Category>>> GetAll();
+
+        Task<IResult> Add(CategoryAddDto category,string CreatedByName);
+
+        Task<IResult> Update(CategoryUpdateDto category,string ModifiedByName);
+        Task<IResult> Delete(int categoryID);
+
+        Task<IResult> HardDelete(int categoryID);
+    }
+
+}
