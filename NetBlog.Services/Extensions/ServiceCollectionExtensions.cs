638ea0d36096f507cdf5e2971a5b0befb46772b8
Semih Özhan <semihozhan@yandex.com>
2022-01-22 14:19:42 +0300

    IServiceCollection

---
+++ NetBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetBlog.Data.Abstract;
+using NetBlog.Data.Concreate;
+using NetBlog.Data.Concreate.EntityFramework.Contexts;
+using NetBlog.Services.Abstract;
+using NetBlog.Services.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions 
+    {
+        public static IServiceCollection LoadMyService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<NetBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+
+            return serviceCollection;
+        }
+    }
+}
